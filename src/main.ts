a6577c4c51ff9c97166a02689619e79c6768b8ae
dankorotenko <danilkorotenko2@gmail.com>
2022-10-26 19:20:08 +0200

    some parts and slider

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,66 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// * IMAGE SLIDER
+const slideBtns: HTMLButtonElement[] | null = Array.from(
+  document.querySelectorAll<HTMLButtonElement>("[data-slide-btn]")
+);
+const slideContainer = document.querySelector<HTMLElement>(
+  "[data-slide-container]"
+);
+const slides = [...document.querySelectorAll("[data-slide]")];
+let currentIndex = 0;
+let isMoving = false;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+/**
+ * Handler for Button Click
+ *
+ * @param {*} e
+ * @return {*} void
+ */
+function handleSlideBtnClick(e: any): void {
+  if (isMoving) return;
+  isMoving = true;
+  e.currentTarget.id === "prev" ? currentIndex-- : currentIndex++;
+  slideContainer?.dispatchEvent(new Event("slidermove"));
+}
+
+//remover/add attribute function
+const removeDisabledAttribute = (els: HTMLElement[]) =>
+  Array.from(els).forEach((el) => el.removeAttribute("disabled"));
+const addDisabledAttribute = (els: HTMLElement[]) =>
+  Array.from(els).forEach((el) => el.setAttribute("disabled", "true"));
+
+// event listneers
+slideBtns.forEach((btn) => btn.addEventListener("click", handleSlideBtnClick));
+
+slideContainer?.addEventListener("slidermove", () => {
+  // 1. translate container to the right/left
+  slideContainer.style.transform = `translateX(-${
+    currentIndex * slides[0].clientWidth
+  }px)`;
+  // 2. remove disabled attributes
+  removeDisabledAttribute(slideBtns);
+  // 3. renable disbaled attributes if needed
+  currentIndex === 0 && addDisabledAttribute([slideBtns[0]]);
+});
+
+//transition end event
+slideContainer?.addEventListener("transitionend", () => (isMoving = false));
+
+//disable image drag event
+const images: HTMLImageElement[] = Array.from(
+  document.querySelectorAll("[data-slide] img")
+);
+images.forEach((img: HTMLImageElement) => (img.ondragstart = () => false));
+
+// intersection observer for slides
+const slideObserver = new IntersectionObserver(
+  (slide) => {
+    if (slide[0].isIntersecting) {
+      addDisabledAttribute([slideBtns[1]]);
+    }
+  },
+  { threshold: 0.75 }
+);
+
+slideObserver.observe(slides[slides.length - 1]);
